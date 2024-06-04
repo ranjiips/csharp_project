d1f5e1c62f4e1d9efa6f4f51131158c6d1007215
ranjiips <ranji_ips@yahoo.com>
2024-06-04 22:57:03 +0530

    Updated the changes and push to GIT

--- ExploreCSharp/obj/Debug/net5.0/ExploreCSharp.AssemblyInfo.cs
+++ ExploreCSharp/obj/Debug/net5.0/ExploreCSharp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExploreCSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31114e3f5293c5b52233ccc60abcc9e64645b600")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20ae118c936043be0154888423fea9b2397fe3c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExploreCSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExploreCSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
