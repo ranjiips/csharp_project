1f70da34f8564ed6eb4425544be09331e24ce053
ranjiips <ranji_ips@yahoo.com>
2024-07-08 19:02:53 +0530

    Updated into GIT

--- SpecFlowProject1_old/obj/Debug/net48/SpecFlowProject1.AssemblyInfo.cs
+++ SpecFlowProject1_old/obj/Debug/net48/SpecFlowProject1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SpecFlowProject1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4dad8856b3f9b6e46c80e6de54d70093841d909a")]
 [assembly: System.Reflection.AssemblyProductAttribute("SpecFlowProject1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SpecFlowProject1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
