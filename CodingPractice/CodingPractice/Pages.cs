d2383f2d6501699738252d99219afa9395812e6e
ranjiips <ranji_ips@yahoo.com>
2024-07-08 18:52:41 +0530

    Updated into GIT

---
+++ CodingPractice/CodingPractice/Pages.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodingPractice.HackerRank;
+
+namespace CodingPractice
+{
+    public class Pages
+    {
+        private static HackerRankProblems _hackerRankProblems;
+        public static HackerRankProblems HackerRankProblems => _hackerRankProblems ?? (_hackerRankProblems = new HackerRankProblems());
+    }
+}
